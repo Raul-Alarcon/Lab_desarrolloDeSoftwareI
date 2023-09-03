ab639f9ee2913c7573d11dc6a8d34fa0aee07051
Raul-Alarcon <mongeraul281@gmail.com>
2023-09-03 16:20:34 -0600

    segundo Ejercico guia 1

---
+++ ejercico2g1/src/main/java/com/mycompany/ejercico2g1/Ejercico2g1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercico2g1;
+
+/**
+ *
+ * @author Raul
+ */
+public class Ejercico2g1 {
+
+    public static void main(String[] args) {
+        screen pantalla = new screen();
+    }
+}
