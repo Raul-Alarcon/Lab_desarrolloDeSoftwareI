df075c79a99174e79ad3b7627f4840c29f131fc1
Raul-Alarcon <mongeraul281@gmail.com>
2023-09-03 16:31:22 -0600

    ejercicio 1

---
+++ g1/ejercico1g1/src/main/java/com/mycompany/swingtarea/SwingTarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.swingtarea;
+
+/**
+ *
+ * @author A21-PC11
+ */
+public class SwingTarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
